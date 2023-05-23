4f498fb6408c236bd6bc607fd860a41eb017c71b
joyanhui <40133116+joyanhui@users.noreply.github.com>
2023-05-23 11:00:12 +0800

    nc/config/config.php 添加了注释

--- nextcloud/config.php
+++ nextcloud/config.php
@@ -5,37 +5,37 @@ $CONFIG = array (
   'secret' => 'XXXXXXX/XXXXXXXXXXXXXX/XXXXXXXXXX',
   'trusted_domains' =>
    [
-    '10.1.1.203:9901',
+    '10.1.1.203:9901',  //可信任域名 可以不带端口
     'XXXX.XXXX.leiyanhui.com',
     'XXXXXXXXXXX.eu.org',
     '[2001:db8::1]'
   ],
-  'overwriteprotocol' => 'http',
-  'datadirectory' => '/www/wwwroot/nextcloud.com/data',
+  'overwriteprotocol' => 'http', //如果要开启后https 或者反向代理https 这里都要修改成https
+  'datadirectory' => '/www/wwwroot/nextcloud.com/data',  //数据库储存目录，可以挪走，也可以直接在 用户名/files 位置 ln一个软连接
   'dbtype' => 'mysql',
   'version' => '26.0.1.1',
-  'overwrite.cli.url' => 'http://10.1.1.203:9901',
+  'overwrite.cli.url' => 'http://10.1.1.203:9901',  //这个如果走反向代理最好修改了实际的url
   'dbname' => 'nextcloud',
   'dbhost' => 'localhost',
   'dbport' => '',
   'dbtableprefix' => 'oc_',
   'mysql.utf8mb4' => true,
   'dbuser' => 'nextcloud',
-  'dbpassword' => 'XXXXXXXXXXXXXXXXXX',
+  'dbpassword' => 'XXXXXXXXXXXXXXXXXX', //数据库密码
   'installed' => true,
   'default_phone_region' => 'CN',
   'filesystem_check_changes' => true,
   'localstorage.allowsymlinks' => true,
-  'memcache.local' => '\\OC\\Memcache\\Redis',
+  'memcache.local' => '\\OC\\Memcache\\Redis',  //没用Memcached而且是用的Memcache和redis
   'memcache.distributed' => '\\OC\\Memcache\\Redis',
   'filelocking.enabled' => 'true',
   'memcache.locking' => '\\OC\\Memcache\\Redis',
-  'redis' => 
+  'redis' =>  //如果有密码，添加password 也可以指定db 也可以 sock
   array (
     'host' => '127.0.0.1',
-    'port' => 6379,
+    'port' => 6379, 
   ),
-  'mail_from_address' => 'XXXXX',
+  'mail_from_address' => 'XXXXX', //邮箱部分配置，这部分后台配置也可以，会复写这里
   'mail_smtpmode' => 'smtp',
   'mail_sendmailmode' => 'smtp',
   'mail_domain' => 'qq.com',
