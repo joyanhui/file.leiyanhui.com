43181f0720ba701b713c7f7f427d6ffd7a2fc5f7
joyanhui <40133116+joyanhui@users.noreply.github.com>
2023-09-29 14:00:48 +0800

    初始化

---
+++ pve-igpu-hooks/vm-start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+VMID="$1"
+igd_id="8086 $(lspci -n|grep '0:02.0'|cut -d ':' -f4|cut -c 1-4)"
+
+echo "VM $VMID is starting" > $(dirname $0)/$VMID-hooks.log
+
+#$(dirname $0)/vfio-startup.sh
+/opt/pve-igpu-hooks/vfio-startup.sh
+
+sleep 1
+
+echo 0000:00:02.0 > /sys/bus/pci/drivers/i915/unbind
+if ! lsmod | grep "vfio_pci" &> /dev/null ; then
+    modprobe vfio-pci
+fi
+echo $igd_id > /sys/bus/pci/drivers/vfio-pci/new_id
---
+++ pve-igpu-hooks/vm-stop.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+VMID="$1"
+ia_addr="0000:$(lspci|grep 'Audio'|grep 'Intel'|cut -c 1-7)"
+usb_addr="0000:$(lspci|grep 'USB'|grep 'Intel'|cut -c 1-7)"
+igd_id="8086 $(lspci -n|grep '0:02.0'|cut -d ':' -f4|cut -c 1-4)"
+
+echo "waitting" >> $(dirname $0)/$VMID-hooks.log
+
+sleep 10
+
+TimeSec=0
+until ! test -e "/var/run/qemu-server/$VMID.pid"
+do
+    if [ $[$TimeSec%3600] -eq 0 ]; then
+        echo "VM $VMID is running "$(date "+%Y-%m-%d %H:%M:%S") >> $(dirname $0)/$VMID-hooks.log
+    fi
+    sleep 3
+    let TimeSec+=3
+done
+
+echo 0000:00:02.0 > /sys/bus/pci/drivers/vfio-pci/unbind
+echo $ia_addr > /sys/bus/pci/drivers/vfio-pci/unbind
+#echo $usb_addr > /sys/bus/pci/drivers/vfio-pci/unbind
+echo $igd_id > /sys/bus/pci/drivers/vfio-pci/remove_id
+echo 0000:00:02.0 > /sys/bus/pci/drivers/i915/bind
+echo $ia_addr > /sys/bus/pci/drivers/snd_hda_intel/bind
+#echo $usb_addr >/sys/bus/pci/drivers/xhci_hcd/bind
+
+sleep 1
+
+#$(dirname $0)/vfio-teardown.sh
+/opt/pve-igpu-hooks/vfio-teardown.sh
+
+echo "VM $VMID stopped "$(date "+%Y-%m-%d %H:%M:%S") >> $(dirname $0)/$VMID-hooks.log
